10b5fce390a0b5feea3fc30975da9a26203a4234
ponlawat koeisuwan <ponlawat6001@gmail.com>
2024-04-25 16:25:24 +0700

    del

--- version-uat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed -i "s/#npa_api_ver#/$1/" uat-compose.yml
-sed -i "s/#pamco_ver#/$2/" uat-compose.yml
-sed -i "s/#property_api_ver#/$3/" uat-compose.yml
-sed -i "s/#npa_cms_ver#/$4/" uat-compose.yml
-sed -i "s/#npa_web#/$5/" uat-compose.yml
--- version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed -i "s/#npa_api_ver#/$1/" docker-compose.yml
-sed -i "s/#pamco_ver#/$2/" docker-compose.yml
-sed -i "s/#property_api_ver#/$3/" docker-compose.yml
-sed -i "s/#npa_cms_ver#/$4/" docker-compose.yml
-sed -i "s/#npa_web#/$5/" docker-compose.yml
